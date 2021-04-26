881ddcd2969433cb1b3ff549a566d7247e75c9c6
Meet-creator <73884094+Meet-creator@users.noreply.github.com>
2021-04-26 15:55:29 +0530

    Add files via upload

---
+++ Printing_Positive_Numbers.py
@@ -0,0 +1,9 @@
+list1 = [12,-7,5,64,-14] 
+for num in list1:
+    if num >= 0:
+        print("List 1=> ",num,end = " ")
+
+list2 = [12,14,-95,3]
+for num in list2:
+    if num >= 0:
+        print("List 2 => ",num,end = " ")
